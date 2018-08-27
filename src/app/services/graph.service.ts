6bb464193b1f81c8504d24c7c848461fadeaa4bc
Arnaud Rachez <arnaud.rachez@gmail.com>
2018-08-27 13:22:17 +0200

    Initial commit

---
+++ src/app/services/graph.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GraphService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getNodes() {
+    const url = 'http://localhost:5000/nodes';
+    return this.httpClient.get(url);
+  }
+
+  getAdjancy() {
+    const url = 'http://localhost:5000/adjancy';
+    return this.httpClient.get(url);
+  }
+}
