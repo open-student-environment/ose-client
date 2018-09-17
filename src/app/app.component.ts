831009c048792f8a73cf6032b85d44fff132021f
Arnaud Rachez <arnaud.rachez@gmail.com>
2018-09-17 17:09:34 +0200

    issue #13 - profile page skeleton * ENH Resonsive navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { tap } from 'rxjs/operators';
-
 import { GraphService } from './services/graph.service';
 import { SchoolsService } from './services/schools.service';
-import { Observable, of, BehaviorSubject } from 'rxjs';
 import { GraphComponent } from './graph/graph.component';
-
-const ADJ = {
-  '1': [ 7,  2,  6],
-  '2': [ 8,  3,  1],
-  '3': [ 4,  2,  9],
-  '4': [10,  3,  5],
-  '5': [ 6,  4, 11],
-  '6': [ 1,  5, 12],
-  '7': [ 1, 20, 13],
-  '8': [ 2, 14, 21],
-  '9': [ 3, 22, 15],
- '10': [ 4, 16, 23],
- '11': [ 5, 17, 24],
- '12': [ 6, 19, 18],
- '13': [ 7, 19],
- '14': [20,  8],
- '15': [21,  9],
- '16': [22, 10],
- '17': [23, 11],
- '18': [24, 12],
- '19': [12, 13],
- '20': [14,  7],
- '21': [ 8, 15],
- '22': [ 9, 16],
- '23': [10, 17],
- '24': [11, 18]
-};
-
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,21 @@ import { DraggableDirective, ZoomableDirective } from './d3/directives';
 
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatChipsModule } from '@angular/material/chips';
 
 import { GraphService } from './services/graph.service';
 import { SchoolsComponent } from './schools/schools.component';
 import { ClickOutsideDirective } from './directives/click-outside.directive';
-import { MatIconModule, MatButtonModule } from '@angular/material';
+import { MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule } from '@angular/material';
 import { ChartsComponent } from './charts/charts.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -34,13 +37,18 @@ import { ChartsComponent } from './charts/charts.component';
     SchoolsComponent,
     ClickOutsideDirective,
     ChartsComponent,
+    ProfileComponent,
+    LayoutComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatTableModule, MatCheckboxModule, MatPaginatorModule, MatButtonModule,
-    MatFormFieldModule, MatInputModule, MatChipsModule, MatIconModule
+    MatFormFieldModule, MatInputModule, MatChipsModule, MatIconModule,
+    MatToolbarModule, MatSidenavModule, MatListModule,
+    AppRoutingModule,
   ],
   providers: [
     HttpClient,
